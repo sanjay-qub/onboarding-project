3a67ccb3fa3fc861cb226e10ff5be9ed297577a3
Sanjay Qubryx <sanjayqubryx@Sanjays-MacBook-Air.local>
2023-11-29 19:43:30 +0530

    quiz  backend  commit

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,6 @@ const express=require('express');
 const router = express.Router();
 const userContoller=require("../controllers/userController")
 
-// router.post("/create",userContoller.createUser)
-// router.put("/edit",userContoller.editUser)
-// router.delete("/delete",userContoller.deleteUser)
-// router.get("/get",userContoller.findAll)
 router.post("/saveResult",userContoller.saveResult)
 router.get("/get",userContoller.findAll)
 
